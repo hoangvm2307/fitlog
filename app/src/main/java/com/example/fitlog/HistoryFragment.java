266aba98ca4640527ecc33596f9ad029e8c32b5d
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2024-10-05 20:26:04 +0700

    fix

--- app/src/main/java/com/example/fitlog/HistoryFragment.java
+++ app/src/main/java/com/example/fitlog/HistoryFragment.java
@@ -31,6 +31,9 @@ public class HistoryFragment extends Fragment {
         dbHelper = DatabaseHelper.getInstance(getContext());
         
         // Check if the database is empty before seeding
+        if (isDatabaseEmpty()) {
+            dbHelper.seedData();
+        }
 
         loadWorkoutHistory();
 
