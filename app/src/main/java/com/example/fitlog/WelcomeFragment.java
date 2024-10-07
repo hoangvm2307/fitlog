1ad086e8f52c03c6c1686322eba9462212311382
Bao Ngoc <ngocnguyen.150903@gmail.com>
2024-10-08 01:03:12 +0700

    Implement welcome fragment

---
+++ app/src/main/java/com/example/fitlog/WelcomeFragment.java
@@ -0,0 +1,41 @@
+package com.example.fitlog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class WelcomeFragment extends Fragment {
+
+    private Button btnNext;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnNext = view.findViewById(R.id.btnContinue);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity() != null) {
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new LoginFragment())
+                            .addToBackStack(null)
+                            .commit();
+                }
+            }
+        });
+    }
+}
