eb860ad533b212e81db1df11a4a1bb3b0337d7aa
Vo Minh Khoi <vominhkhoi12032003@gmail.com>
2024-10-08 11:31:24 +0700

    update template model

--- app/src/main/java/com/example/fitlog/model/Template.java
+++ app/src/main/java/com/example/fitlog/model/Template.java
@@ -1,12 +1,22 @@
-package com.example.fitlog;
+package com.example.fitlog.model;
+
+import java.time.LocalDateTime;
 
 public class Template {
     private String title;
     private String description;
     private String lastUsed;
+    private String visibility;
+    private LocalDateTime createAt;
+    private LocalDateTime lastUsed;
 
-    public Template(String title, String description, String lastUsed) {
-        this.title = title;
+    public Template() {
+    }
+
+    public Template(int id, int userId, String name, String description, String visibility, LocalDateTime createAt) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
         this.description = description;
         this.lastUsed = lastUsed;
     }
@@ -20,6 +30,32 @@ public class Template {
     }
 
     public String getLastUsed() {
-        return lastUsed;
+        return this.lastUsed.toString();
+    }
+
+    public void setLastUsed(LocalDateTime lastUsed) {
+        this.lastUsed = lastUsed;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
     }
 }
+
